c9f29321e363f4db83f662dcc83b95344eec24b3
Guilherme <guiglreis@gmail.com>
2026-01-30 21:27:16 -0300

    Criado a controller - a2

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/Controller.java
@@ -0,0 +1,14 @@
+package dev.java10x.CadastroDeNinjas;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping
+
+public class Controller {
+
+    @GetMapping("/boasVindas")
+    public String boasVindas() {
+        return "Essa é minha primeira mensagem nessa rota";
+    }
+}
