775bd278d61fe79906e0e5d04e2099005cf6f447
Guilherme <guiglreis@gmail.com>
2026-02-05 21:12:44 -0300

    feat: add lombok to abstract class

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -2,11 +2,17 @@ package dev.java10x.CadastroDeNinjas.Missoes;
 
 import dev.java10x.CadastroDeNinjas.Ninjas.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_missoes")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class MissoesModel {
 
     @Id
@@ -18,28 +24,4 @@ public class MissoesModel {
     //@OneToMany -> Uma missao pode ter varios ninjas
     @OneToMany(mappedBy = "missoes")
     private List<NinjaModel> ninjas;
-
-    public MissoesModel() {
-    }
-
-    public MissoesModel(String nomeMissao, char rankMissao) {
-        this.nomeMissao = nomeMissao;
-        this.rankMissao = rankMissao;
-    }
-
-    public String getNomeMissao() {
-        return nomeMissao;
-    }
-
-    public void setNomeMissao(String nomeMissao) {
-        this.nomeMissao = nomeMissao;
-    }
-
-    public char getRankMissao() {
-        return rankMissao;
-    }
-
-    public void setRankMissao(char rankMissao) {
-        this.rankMissao = rankMissao;
-    }
 }
