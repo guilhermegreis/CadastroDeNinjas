24bba460f161245a27593a3073b2df3f83632301
Guilherme <guiglreis@gmail.com>
2026-02-24 19:38:20 -0300

    Query

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -11,4 +11,35 @@ public class NinjaController {
     public String boasVindas() {
         return "Essa é minha primeira mensagem nessa rota";
     }
+
+    //Adicionar ninja (CREATE)
+    @PostMapping("/criar")
+    public String criarNinja() {
+        return "Ninja criado";
+    }
+
+    //Mostrar todos os ninjas (READ)
+    @GetMapping("/todos")
+    public String mostrarTodosOsNinjas() {
+        return "Mostrar Ninja: ";
+    }
+
+    //Mostrar ninja por id (READ)
+    @GetMapping("/todosID")
+    public String mostrarTodosOsNinjasPorID() {
+        return "Mostrar Ninja por ID: ";
+    }
+
+    //Alterar dados dos ninjas (UPDATE)
+    @PutMapping("/alterarID")
+    public String alterarNinjaPorID() {
+        return "Alterar Ninja por ID: ";
+    }
+
+    //Deletar ninjas (DELETE)
+    @DeleteMapping("deletarID")
+    public String deletarNinjaPorID() {
+        return "Ninja deletado";
+    }
+
 }
